aeafc16ee917b3bb411113dbd65217f116718290
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-06-17 06:40:03 +0530

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,15 @@
+# Hello-Hi Program in Ruby
+
+print("Enter the text: ")
+myVar = gets.chomp
+
+if myVar == "Hello"
+    print "Hi, how are you ?"
+    
+elsif myVar == "Hi"
+    print "Hello, how are you ?"
+
+else
+    print "I have absolutely no idea what you are trying to convey 😢"
+
+end
